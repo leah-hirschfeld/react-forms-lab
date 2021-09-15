4a1d44e010982bce51f4df61dd35c73ba809129a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2021-09-15 07:52:01 -0700

    commit tree f45789390e240541d6b9b38f49d90a006a52d273

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
+    const charsLeft = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+        type="text" 
+        name="message" 
+        value={this.state.message}
+        onChange={this.handleChange}
+        id="message" />
+        Characters left: {charsLeft}
       </div>
     );
   }
